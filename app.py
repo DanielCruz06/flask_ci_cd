b29a145b9c9b33e04b6589058aed516f38fe020a
Jose Daniel Cruz <josedanielcruz@Joses-MacBook-Air.local>
2025-08-07 09:03:33 -0500

    cambios en texto

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Home Page!, by branch developer!"
+    return "Welcome to the Home Page!, cambio desde codigo - rama developer" \
+    ", cambio desde codigo - rama developer 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
