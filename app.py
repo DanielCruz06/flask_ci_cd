2848b5499348a6b6bfb0d3da477f4eb064ffe1da
Jose Daniel Cruz <josedanielcruz@Joses-MacBook-Air.local>
2025-08-07 22:58:11 -0500

    se justa comentario en pagina

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Welcome to the Home Page!, cambio desde codigo - rama developer" \
-    ", cambio desde codigo - rama developer 2"
+    ", cambio desde codigo - rama developer 2" \
+    "prueba cambio desde rama developer 3"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
